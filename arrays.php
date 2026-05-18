2b8a01f20e022b74f9136c91cdeac6eb5a55df2b
Joel T Cunha <joel.cunha@educacao.mg.gov.br>
2026-05-18 19:25:41 -0300

    alteração no array e BD

--- arrays.php
+++ arrays.php
@@ -1,38 +1,78 @@
 <?php
 
-//  ARRAYS
+//  ARRAYS (MATRIZ)
 //  É uma variável especial que pode manter muitos valores sob um único nome e é possível acessar os valores referindo-se a eles com um número de índice ou um nome.
+//  Os itens de um array podem ser qualquer tipo de dados
 
-$carros = array("Volvo", "BMW", "Toyota");
+// CRIANDO ARRAYS
+echo "Meu array <br>";
+$meuArray= array("Volvo", 15,  ["maçãs", "bananas"]);
+var_dump($meuArray);
 echo "<br>";
-var_dump($carros);
 
+echo "Meu segundo array <br>";
+$meuSegundoArray = ["Volvo", 15,  ["maçãs", "bananas"]];
+var_dump($meuSegundoArray);
+echo "<br>";
+
+$names = [
+    "John",
+    "Mary",
+    "Jane",
+    "All are Does"
+];
+var_dump($names);
+
+echo "<br>";
+//  Declarando um array vazio
+$cities = [];
+$cities[0] = "Londres";
+$cities[1] = "Asturias";
+$cities[2] = "Contagem";
+var_dump($cities);
+echo "<br>";
 //  TRÊS TIPOS DE ARRAYS
 //  -> Indexadas
 //  -> Associativas
 //  -> Multidimensional
 
-//  Os itens de um array podem ser qualquer tipo de dados
+//  ARRAYS INDEXADOS
+//  Em um array indexado cada item tem um índice numérico.
 
-//$myArr = array("Volvo", 15,  ["apples", "bananas"]);
-//echo "$myArr[0]<br>";
-//echo "$myArr[1]<br>";
-//echo "$myArr[2]";
+echo  "Arrays Indexados: <br>";
+$carros = array("Volvo", "BMW", "Toyota");
+echo "<br>";
+var_dump($carros);
+echo "<br><br>";
+echo "Acessando item do array: <br>";
+echo "Posição [0] = " . $carros[0] . "<br>";
 
-// Contando os itens de um array
-
-echo count($carros);
-
-$cars = array("Volvo", "BMW", "Toyota");
-echo "<br>" . $cars[0];
+echo "<br><br>";
 
 //  MUDANDO O VALOR DE UM ITEM DO ARRAY
+//  Para mudar o valor de um item, utilize o índice numérico
+
+echo "Mudando o valor de um array: <br>";
 $animals = array("dog", "cat", "horse", "monkey");
 echo "<br>";
 var_dump($animals);
 echo "<br>";
+echo "Substituindo cat por Bat";
 $animals[1] = "Bat";
 var_dump($animals);
+
+// LOOP ATRAVÉS DE UM ARRAY INDEXADO
+echo "<br><br>";
+echo "Usando a array animals <br>";
+echo "Usa-se foreach (para cada) para percorrer o array <br>";
+foreach ($animals as $animal) {
+    echo $animal . "<br>";
+}
+echo "<br><br>";
+// Contando os itens de um array
+echo "Contando os itens de um array <br>";
+echo count($carros);
+echo "<br><br>";
 
 //  Percorrer a array e imprimir todos os valores
 $animals = array("dog", "cat", "horse", "monkey");
@@ -41,19 +81,22 @@ foreach ($animals as $animal) {
 }
 echo "<br><br>";
 
-
 //      ARRAYS ASSOCIATIVAS
-//  Usa uma chave nomeada
+//  Usa uma chave nomeada, ao invés de índices numéricos
+echo "Arrays Associativos: <br>";
 $car = array("marca"=>"Ford", "modelo"=>"Mustang", "ano"=>1964);
 var_dump($car);
 
 // Acessando um item do array
+echo"Acessando o item de um array associativo <br>";
 echo $car["marca"];
 echo "<br>";
-
+echo "<br><br>";
 //  Trocando um item de um array
+echo "Trocando item do array <br>";
 $car["ano"] = 2024;
 var_dump($car);
+echo "<br><br>";
 
 //  Percorrer uma array associativa
 foreach ($car as $x => $y) {
@@ -73,4 +116,3 @@ $myCar["model"] = "Variant";
 $myCar["year"] = 1970;
 var_dump($myCar);
 
-//  ADICIONAR ITENS AO ARRAY
---
+++ exercicios_array.php
@@ -0,0 +1,13 @@
+<?php
+//  EXERCÍCIO
+//  Qual a sintáxe correta para criar um array em PHP?
+
+
+//  EXERCÍCIO
+// Na seguinte matriz:
+// $fruits = array('Apple', 'Banana', 'Orange');
+// Qual seria a sintaxe correta para alterar o segundo valor de 'Banana' para 'Pineapple'?
+
+
+//  EXERCÍCIO
+//  Exiba o segundo item da matriz $fruits.
