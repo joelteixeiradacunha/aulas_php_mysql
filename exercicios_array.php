d2f8b74eef233ff026a1e2d4aaf2969cf1fa3d63
joelteixeiradacunha <joel.cunha@educacao.mg.gov.br>
2026-06-01 12:11:20 -0300

    arrays 01jun2026

--- exercicios_array.php
+++ exercicios_array.php
@@ -11,3 +11,23 @@
 
 //  EXERCÍCIO
 //  Exiba o segundo item da matriz $fruits.
+
+// EXERCÍCIO 1
+//$fruits = array('Tamarindo', 'Pitaia', 'Siriguela');
+//Qual a sintax correta para mudar o segundo valor para Abacaxi?
+
+//  EXERCÍCIO 2
+//  Como imprimir o terceiro item do array $fruits?
+
+//  EXERCÍCIO 3
+//  Crie um array associativo para estado/capital/data de fundação da capital?
+
+//  EXERCÍCIO 4
+// Dada a array associativa abaixo. Como imprimir a idade de Ben?
+//  $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+//  EXERCÍCIO 5
+//  Preencha os espaços em branco para imprimir a chave e o valor.
+// ________($idade ___ $x ___ $y){
+// echo "Chave=" . ____ . ", Valor=" . ____;
+//  }
