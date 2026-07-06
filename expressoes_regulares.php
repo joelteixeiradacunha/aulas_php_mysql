0a719def66f78e49ab278ba2e377f095dc45b72e
Joel T Cunha <joel.cunha@educacao.mg.gov.br>
2026-07-06 17:05:40 -0300

    Expressões regulares

--- expressoes_regulares.php
+++ expressoes_regulares.php
@@ -136,4 +136,294 @@ echo preg_replace($pattern, "#", $txt);
 echo "<br>===================================================<br>";
 ?>
 
+//  [A-Z]
+
+
+<p>Quantas letras no texto "E.E. Maria Luiza Miranda Bastos" estão alfabeticamente entre "A" maiúsculo e "Z" maiúculo:</p>
+
+<?php
+
+$txt = "Bem vindos ao E.E. Maria Luiza Miranda Bastos";
+$pattern = "/[A-Z]/";
+echo preg_match_all($pattern, $txt);
+?>
+
+<p>As correspondências são mostradas aqui:</p>
+
+<?php
+echo preg_replace($pattern, "#", $txt);
+?>
+
+<p>Cada correspondência foi trocada por #</p>
+
+<?php
+echo "<br>===================================================<br>";
+?>
+//  [0-5] [0-9]
+
+<p>Quantos caracteres no texto "Ligue 99561-1098" é um dígito entre "0" e "5"?</p>
+
+<?php
+$txt = "Ligue 99561-1098";
+$pattern = "/[0-5]/";
+echo preg_match_all($pattern, $txt);
+?>
+
+<p>As correspondências são mostradas aqui:</p>
+
+<?php
+echo preg_replace($pattern, "#", $txt);
+?>
+
+<p>Cada correspondência foi trocada por #</p>
+
+<?php
+echo "<br>===================================================<br>";
+?>
+
+<p>Quantos caracteres no texto "Ligue 99561-1098" é um dígito entre "0" e "9"?</p>
+
+<?php
+$txt = "Ligue 99561-1098";
+$pattern = "/[0-9]/";
+echo preg_match_all($pattern, $txt);
+?>
+
+<p>As correspondências são mostradas aqui:</p>
+
+<?php
+echo preg_replace($pattern, "#", $txt);
+?>
+
+<p>Cada correspondência foi trocada por #</p>
+
+<?php
+echo "<br>===================================================<br>";
+?>
+
+//  |
+
+<p>Quantas ocorrências de "gato", "cachorro" ou "peixe" estão no texto "Nós temos dois gatos, um peixe e nenhum cachorro.":</p>
+
+<?php
+$txt = "Nós temos dois gatos, um peixe e nenhum cachorro.";
+$pattern = "/cachorro|gato|peixe/";
+echo preg_match_all($pattern, $txt);
+?>
+
+<p>As correspondências são mostradas aqui:</p>
+
+<?php
+echo preg_replace($pattern, "#", $txt);
+?>
+
+<p>Cada correspondência foi trocada por #</p>
+
+<?php
+echo "<br>===================================================<br>";
+?>
+
+
+//  ^ (circunflexo)
+
+<p>O texto "Vou me embora para Pasárgada.", começa com "Vou"?:</p>
+
+<?php
+$txt = "Vou me embora para Pasárgada.";
+$pattern = "/^Vou/";
+echo preg_match_all($pattern, $txt);
+?>
+
+<p>Esse método retorna 1 e há uma correpondência, caso contrário 0</p>
+
+<p>As correspondências são mostradas aqui:</p>
+
+<?php
+echo preg_replace($pattern, "#", $txt);
+?>
+
+<p>Cada correspondência foi trocada por #</p>
+
+<?php
+echo "<br>===================================================<br>";
+?>
+
+//  $ (sifrão)
+
+<p>O texto "Porque lá sou amigo do rei", termina com "rei"?:</p>
+
+<?php
+$txt = "Porque lá sou amigo do rei";
+$pattern = "/rei$/";
+echo preg_match_all($pattern, $txt);
+?>
+
+<p>Esse método retorna 1 e há uma correpondência, caso contrário 0</p>
+
+<p>As correspondências são mostradas aqui:</p>
+
+<?php
+echo preg_replace($pattern, "#", $txt);
+?>
+
+<p>Cada correspondência foi trocada por #</p>
+
+<?php
+echo "<br>===================================================<br>";
+?>
+
+
+//  \d
+
+<p>Quantos números estão no texto "O ano de 1945 foi marcado pelo fim da 2º guerra mundial."?:</p>
+
+<?php
+$txt = "O ano de 1945 foi marcado pelo fim da 2º guerra mundial.";
+$pattern = "/\d/";
+echo preg_match_all($pattern, $txt);
+?>
+
+<p>As correspondências são mostradas aqui:</p>
+
+<?php
+echo preg_replace($pattern, "#", $txt);
+?>
+
+<p>Cada correspondência foi trocada por #</p>
+
+<?php
+echo "<br>===================================================<br>";
+?>
+
+
+//  \D
+
+    p>Quantos caracteres não numéricos estão no texto "O ano de 1945 foi marcado pelo fim da 2º guerra mundial."?:</p>
+
+<?php
+$txt = "O ano de 1945 foi marcado pelo fim da 2º guerra mundial.";
+$pattern = "/\D/";
+echo preg_match_all($pattern, $txt);
+?>
+
+    <p>As correspondências são mostradas aqui:</p>
+
+<?php
+echo preg_replace($pattern, "#", $txt);
+?>
+
+    <p>Cada correspondência foi trocada por #</p>
+
+<?php
+echo "<br>===================================================<br>";
+?>
+
+//  \s
+
+<p>Quantos espaços em brancos tem no texto <br> "De tudo, ao meu amor serei atento. Antes, e com tal zelo, e sempre, e tanto que mesmo em face do maior encanto dele se encante mais meu pensamento"?:</p>
+
+<?php
+$txt = "De tudo, ao meu amor serei atento. Antes, e com tal zelo, e sempre, e tanto que mesmo em face do maior encanto dele se encante mais meu pensamento";
+$pattern = "/\s/";
+echo preg_match_all($pattern, $txt);
+?>
+
+<p>As correspondências são mostradas aqui:</p>
+
+<?php
+echo preg_replace($pattern, "#", $txt);
+?>
+
+    <p>Cada correspondência foi trocada por #</p>
+
+<?php
+echo "<br>===================================================<br>";
+?>
+
+//  \S
+<p>Quantos caracteres sem os espaços em branco tem no texto <br> "Quero vivê-lo em cada vão momento e em seu louvor hei de espalhar meu canto e rir meu riso e derramar meu pranto ao seu pesar ou seu contentamento."?:</p>
+
+<?php
+$txt = "Quero vivê-lo em cada vão momento e em seu louvor hei de espalhar meu canto e rir meu riso e derramar meu pranto ao seu pesar ou seu contentamento.";
+$pattern = "/\S/";
+echo preg_match_all($pattern, $txt);
+?>
+
+<p>As correspondências são mostradas aqui:</p>
+
+<?php
+echo preg_replace($pattern, "#", $txt);
+?>
+
+<p>Cada correspondência foi trocada por #</p>
+
+<?php
+echo "<br>===================================================<br>";
+?>
+\\  \w
+
+<p>Quantos caracteres alfabéticos ou numéricos estão no texto "www.devops.com.br?:</p>
+<?php
+$txt = "www.devops.com.br";
+$pattern = "/\w/";
+echo preg_match_all($pattern, $txt);
+?>
+
+<p>As correspondências são mostradas aqui:</p>
+
+<?php
+echo preg_replace($pattern, "#", $txt);
+?>
+
+    <p>Cada correspondência foi trocada por #</p>
+
+<?php
+echo "<br>===================================================<br>";
+?>
+
+
+//  \W
+
+<p>Qunatos caracteres não-alfabéticos e não-numéricos estão no texto "www.devops.com.br"?:</p>
+<?php
+$txt = "www.devops.com.br";
+$pattern = "/\W/";
+echo preg_match_all($pattern, $txt);
+?>
+
+<p>As correspondências são mostradas aqui:</p>
+
+
+<?php
+echo preg_replace($pattern, "#", $txt);
+?>
+
+    <p>Cada correspondência foi trocada por #</p>
+
+<?php
+echo "<br>===================================================<br>";
+?>
+
+//  \b
+
+<p>O texto "Que não seja imortal, posto que é chama, mas que seja infinito enquanto dure." começa com "Que"?:</p>
+
+<?php
+$txt = "Que não seja imortal, posto que é chama, mas que seja infinito enquanto dure.";
+$pattern = "/\bQue/";
+echo preg_match_all($pattern, $txt);
+?>
+
+<p>As correspondências são mostradas aqui:</p>
+
+<?php
+echo preg_replace($pattern, "#", $txt);
+?>
+
+    <p>Cada correspondência foi trocada por #</p>
+
+<?php
+echo "<br>===================================================<br>";
+?>
+
 
--- strings.php
+++ strings.php
@@ -2,48 +2,61 @@
 //  É UMA SEQUENCIA DE CARACTERES
 //  É CERCADA POR ASPAS DUPLAS OU SIMPLES
 
-echo "Hello folks";
+echo "Olá terceirão!!!";
 echo '<br>_________________________________________<br>';
 
 //   HÁ DIFERENÇA ENTRE AS DUAS
 //   Uma string entre aspas duplas substituirá o valor das variáveis e aceita muitos caracteres especiais, como \n, \r, \t,.
 
-echo "Exemplo: <br>";
+echo "Exemplo: aspas duplas<br>";
 
-$x = "John Doe";
-$y = "Jane Doe";
-echo "Hello $x e $y";
+$x = "Christian";
+$y = "Domynic";
+echo "O $x e a $y são muito amigos.";
 echo '<br>_________________________________________<br>';
 
 // AS ASPAS SIMPLES NÃO SUBSTITUEM O VALOR DAS VARIÁVEIS
 // IMPRIME A STRING COMO ELA FOI ESCRITA
 
-//$x = "John Doe";
-//echo 'Hello $x';
-//echo '<br>_________________________________________<br>';
+echo "Exemplo: aspas simples<br>";
+
+$x = "Christian";
+$y = "Domynic";
+echo 'O $x e a $y são muito amigos.';
+echo '<br>_________________________________________<br>';
+
 //
-//  FUNÇÕES COM STRING
+// FUNÇÕES COM STRING
 // STRLEN() RETORNA O TAMANHO DA STRING
 
-echo ("Hello Folks!<br>");
-echo strlen("Hello Folks!");
+echo "Exemplo: strlen()<br>";
+echo ("Olá terceirão. Blz?<br>");
+echo strlen("Olá terceirão. Blz?") . " caracteres.";
 
 echo '<br>_________________________________________<br>';
+
+
 // STR_WORD_COUNT conta o núumero de palavras em uma string
-echo  str_word_count("Hello Folks!");
+echo "Exemplo: str_word_count()<br>";
+echo  str_word_count("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquet velit luctus finibus faucibus. Etiam nisi eros, congue ac sapien.") . " palavras.";
 echo '<br>_________________________________________<br>';
 
 //// STR_CONTAINS verifica se a string contém uma substring específica
-//// RETORNA TRUE OU FALSE
-$txt = "Is that all folks!";
-var_dump(str_contains($txt, "all"));
+//// RETORNA TRUE (1) OU FALSE (0)
+
+echo "Exemplo: str_contains()<br>";
+$txt = "No meio do caminho tinha uma pedra. Tinha uma pedra no meio do caminho. Tinha uma pedra no meio do caminho tinha uma pedra.";
+var_dump(str_contains($txt, "Tinha"));
 echo '<br>_________________________________________<br>';
 var_dump(str_contains($txt, "boy"));
 //
-echo '<br>_________________________________________<br>';
-//// STRPOS procura por um texto específico dentro de uma string
-//// Se encontrado, retorna a posição do primeiro caractere do primeiro correspondente
-echo strpos("Cara, que tanto vocês dão risadas?", "dão");
+//echo '<br>_________________________________________<br>';
+// STRPOS procura por um texto específico dentro de uma string
+// Se encontrado, retorna a posição do primeiro caractere do primeiro correspondente
+
+echo "Exemplo: strpos()<br>";
+
+echo strpos("As casas espiam os homens que correm atrás de mulheres. A tarde talvez fosse azul, não houvesse tantos desejos.", "espiam");
 echo '<br>_________________________________________<br>';
 //
 // STR_STARTS_WITH()
@@ -51,13 +64,14 @@ echo '<br>_________________________________________<br>';
 // Se um correspondente é encontrado, retorna true ou false
 // A função é 'case sensitive'
 
-$tct_2 = "I really love you!";
-echo "str_starts_with()";
-var_dump(str_starts_with($tct_2, "I really"));
+echo "Exemplo: str_starts_with()<br>";
+$txt_2 = "As casas espiam os homens que correm atrás de mulheres. A tarde talvez fosse azul, não houvesse tantos desejos.";
+
+var_dump(str_starts_with($txt_2, "As casas"));
 echo '<br>_________________________________________<br>';
 
-var_dump(str_starts_with($tct_2, "i really"));
-echo '<br>_________________________________________<br>';
+var_dump(str_starts_with($txt_2, "as casas"));
+//echo '<br>_________________________________________<br>';
 
 // STR_ENDS_WITH()
 // Verifica se a string termina com a referida substring
@@ -65,65 +79,65 @@ echo '<br>_________________________________________<br>';
 // A função é 'case sensitive'
 
 $tct_2 = "I really love you!";
-echo "str_ends_with()";
-var_dump(str_ends_with($tct_2, "you!"));
-echo '<br>_________________________________________<br>';
-
-var_dump(str_ends_with($tct_2, "You!"));
-echo '<br>_________________________________________<br>';
+//echo "str_ends_with()";
+//var_dump(str_ends_with($tct_2, "you!"));
+//echo '<br>_________________________________________<br>';
+//
+//var_dump(str_ends_with($tct_2, "You!"));
+//echo '<br>_________________________________________<br>';
 
 //  STRREV inverte a string
 
 $text_3 = "socorra-me subi no ônibus em marrocos";
-echo "strrev()<br>";
-echo strrev($text_3);
-echo '<br>_________________________________________<br>';
+//echo "strrev()<br>";
+//echo strrev($text_3);
+//echo '<br>_________________________________________<br>';
 
 // TRIM
 // remove qualquer espaço em branco do início e do fim da frase
 
 $text_4 = "     I do love you!     ";
-echo $text_4 . "<br>";
-echo trim($text_4);
-
-echo '<br>_________________________________________<br>';
+//echo $text_4 . "<br>";
+//echo trim($text_4);
+//
+//echo '<br>_________________________________________<br>';
 
 // EXPLODE divide uma string em uma array
 
 $text_5 = "1,2, 3, 4, 5, 6, 7, 8, 9";
 $y = explode(",", $text_5);
-echo "Explode";
-print_r($y);
-echo '<br>_________________________________________<br>';
+//echo "Explode";
+//print_r($y);
+//echo '<br>_________________________________________<br>';
 
 // STRING CONCATENATION
 // PARA CONCATENAR utilize . (ponto)
 
 $text_6 = "I don't wanna be your baby!";
 $text_7 = "I wanna be your lover!";
-echo $text_6 . $text_7 . "<br>";
-echo $text_6 . " " . $text_7 . "<br>";
-echo "$text_6 $text_7";
-echo '<br>_________________________________________<br>';
+//echo $text_6 . $text_7 . "<br>";
+//echo $text_6 . " " . $text_7 . "<br>";
+//echo "$text_6 $text_7";
+//echo '<br>_________________________________________<br>';
 //
 // SUBSTR -> FATIA UMA STRING
 // É usado para extrair uma parte de uma string
 // Especifique o índice inicial e o número de caracteres que você quer que retorne
 
 $text_8 = "Your kiss is on my list of the best things of life!";
-echo substr($text_8, 10, 20) . "<br>";
+//echo substr($text_8, 10, 20) . "<br>";
 //
 //
 // Tirando o último parâmetro , a faixa irá até o final.
-echo substr($text_8, 12) . "<br>";
+//echo substr($text_8, 12) . "<br>";
 //
 // Use índice negativo irá começar a dividir do final para o início
-echo substr($text_8, -15,) . "<br>";
+//echo substr($text_8, -15,) . "<br>";
 //
 // Use tamanho negativo para especificar quantos caracteres omitir, começando do final da string
-echo substr($text_8, 5, -3) . "<br>";
+//echo substr($text_8, 5, -3) . "<br>";
 //
 //ESCAPE CHARACTERS
 // Para inserir caracteres que são ilegais em uma string use um caracter de fuga
 $text_9 = "And could \"never\" tears us apart";
-echo $text_9;
+//echo $text_9;
