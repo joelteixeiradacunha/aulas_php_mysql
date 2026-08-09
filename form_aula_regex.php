a6f3f30b76965787ca2d3d40d6e79e417904b1f5
joelteixeiradacunha <joel.cunha@educacao.mg.gov.br>
2026-08-09 20:20:29 -0300

    Aula validação de formulários

--- form_aula_regex.php
+++ form_aula_regex.php
@@ -9,10 +9,10 @@
 <body>
 <form action="form_resposta_regex.php" method="post">
 
-    Nome: <input type="text" name="nome"><br><br>
-    E-mail: <input type="text" name="email"><br><br>
-    Telefone: <input type="text" name="telefone"><br><br>
-    Webmail: <input type="text" name="url"><br><br>
+    Nome: <input type="text" name="nome" placeholder="Digite o nome completo"><br><br>
+    E-mail: <input type="text" name="email" placeholder="Digite um e-mail válido"><br><br>
+    Telefone: <input type="text" name="telefone" placeholder="Digite o telefone com o DDD. Somente números."><br><br>
+    Webmail: <input type="text" name="url" placeholder="Digite a URL completa"><br><br>
     Gênero: <input type="radio" value="masculino" name="genero">Masculino
     <input type="radio" value="Feminino" name="genero">Feminino
     <input type="radio" value="Não quero informar" name="genero">Não informar<br><br>
--- form_resposta_regex.php
+++ form_resposta_regex.php
@@ -5,27 +5,69 @@
     @$url = $_REQUEST["url"];
     @$genero = $_REQUEST["genero"];
 
-    @$valNome = "";
-    @$valEmail = "";
-    @$valTelefone = "";
-    @$valUrl = "";
-    @$valGenero = "";
+    @$valNome = "/^[a-zA-Z]+(?: [a-zA-Z]+)+$/"; // /^[A-Za-zÀ-ÿ]+(?: [A-Za-zÀ-ÿ]+)+$/u
+    @$valEmail = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+    @$valTelefone = "/^[0-9]{2}[2-9][0-9]{8,9}$/";
+    @$valUrl = "/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)$/";
+
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+
+    <style>
+        body, html {
+            margin: 0;
+            padding: 0;
+            background: #fbd7d7;
+        }
+        h1, h2, h3, h4, h5, h6 {
+            font-family: 'Roboto Condensed', sans-serif;
+            color: #8a2626;
+        }
+        section{
+            width: 400px;
+            height: 400px;
+            padding: 10px;
+            background-color: #94a3b8;
+        }
+    </style>
 </head>
 <body>
-    <h1>Você me apertou!!! Delícia</h1>
-    <?php
-        echo $nome . "<br>";
-        echo $email . "<br>";
-        echo $telefone . "<br>";
-        echo $url . "<br>";
-        echo $genero . "<br>";
 
-    ?>
+    <section>
+        <h1>Dados digitados</h1>
+        <?php
+            if (preg_match($valNome, $nome)) {
+                echo "<h3>Nome: $nome </h3>";
+            }else{
+                echo "<p>A informação que você digitou não atende aos requisitos.</p>
+                        <p>Digite seu nome e sobrenome.</p>";
+            }
+
+            if (preg_match($valEmail, $email)) {
+                echo "<h3>E-mail: $email </h3>";
+            }else{
+                echo "<p>Digite um e-mail válido.</p>";
+            }
+
+            if (preg_match($valTelefone, $telefone)) {
+                echo "<h3>Telefone: $telefone </h3>";
+            }else{
+                echo "<p>Digite um telefone válido.</p>";
+            }
+
+            if (preg_match($valUrl, $url)) {
+                echo "<h3>URL: $url </h3>";
+            }else{
+                echo "<p>Digite uma URL válida</p>";
+            }
+
+            echo "<h3>Gênero: $genero </h3>";
+
+        ?>
+    </section>
 </body>
 </html>
